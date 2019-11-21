a26c04421959cac1a447e4990a4b8af9cbad4340
20kdc <asdd2808@gmail.com>
2019-11-21 20:15:17 +0000

    Actually-actually fix things so HTTP API getGlobal now works (don't bother requiring a context)

--- cmd/internal/api/getGlobal.go
+++ cmd/internal/api/getGlobal.go
@@ -2,16 +2,11 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"github.com/CCDirectLink/CCUpdaterCLI"
-	"github.com/CCDirectLink/CCUpdaterCLI/cmd/internal"
-)
 
-//GlobalModsRequest for incoming list available mods requests
-type GlobalModsRequest struct {
-	Game *string `json:"game"`
-}
+	"github.com/CCDirectLink/CCUpdaterCLI"
+	"github.com/CCDirectLink/CCUpdaterCLI/remote"
+)
 
 //GlobalModsResponse contains a list of available mods
 type GlobalModsResponse struct {
@@ -22,14 +17,9 @@ type GlobalModsResponse struct {
 
 //GetGlobalMods returns all available mods
 func GetGlobalMods(w http.ResponseWriter, r *http.Request) {
-	//var decoder *json.Decoder
-	if r.Method == "POST" {
-		//decoder = json.NewDecoder(r.Body)
-	}
-
 	setHeaders(w)
 
-	mods, err := getGlobalMods(decoder)
+	mods, err := getGlobalMods()
 
 	encoder := json.NewEncoder(w)
 	if err == nil {
@@ -45,23 +35,14 @@ func GetGlobalMods(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func getGlobalMods(decoder *json.Decoder) (map[string]ccmodupdater.PackageMetadata, error) {
-	var dir *string = nil
-	if decoder != nil {
-		var req GlobalModsRequest
-		if err := decoder.Decode(&req); err != nil {
-			return nil, fmt.Errorf("cmd/internal/api: Could not parse request body: %s", err.Error())
-		}
-		dir = req.Game
-	}
-
-	res, err := internal.NewOnlineContext(dir)
+func getGlobalMods() (map[string]ccmodupdater.PackageMetadata, error) {
+	res, err := remote.GetRemotePackages()
 	if err != nil {
 		return nil, err
 	}
 
 	metadata := map[string]ccmodupdater.PackageMetadata{}
-	for k, v := range res.RemotePackages() {
+	for k, v := range res {
 		metadata[k] = v.Metadata()
 	}
 	return metadata, nil
